c6841385593de0015e7d47ea1b7e60f28e1681a3
LijunRio <rioin1998@163.com>
2024-12-19 17:28:23 +0100

    update

---
+++ mgca/models/convirt/run_pretrain.sh
@@ -0,0 +1,3 @@
+# CUDA_VISIBLE_DEVICES=0 python convirt_module.py --gpus 1 --strategy ddp --batch_size 72 --num_workers 16
+
+CUDA_VISIBLE_DEVICES=0 python convirt_module.py --gpus 1 --strategy ddp --batch_size 128 --num_workers 16
